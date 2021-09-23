6ae7a9633514661d27f718c002724dd2b0126dad
hakim <bmsuhakim@gmail.com>
2021-09-23 14:25:06 +0600

    up to github

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     <div className="container">
       <HomeText className="row">
         <div className="col-md-8">
-          <h1 className="display-4">This is our blogs home page 2</h1>
+          <h1 className="display-4">This is our blogs home page</h1>
           <p className="lead">Here, you can read and create your own blog.</p>
         </div>
       </HomeText>
